d3cd2ba37b2366d2d814954f780bd87435f534f0
Harsh044-code <harshraj2906@gmail.com>
2024-04-07 12:09:48 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"Use strict"; // treat all JS code as newer version
+
+// alert(3+3) // will pose an error as using alert has a diffrent syntax in node in "browser it will run"
+
+let name= "hitesh"      
+let age = 18           
+let isLoggedIn = false 
+
+// string
+// number => 2 to pwr 53
+// boolean => true/ false
+// null => standalone value not undefined it is used when we don't want 0 but still want it to be empty
+// undefined
+// symbol => will use in react (uniqueness)
+
+
+// object
+
+console.log(typeof"hitesh"); // string
+console.log(typeof null);// null is an object(asked in interviews)
+console.log(typeof undefined); // o/p undefined
